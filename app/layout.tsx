78b4d32823d41f7a31c89031ee9fdb2836da7b21
Pawan Kamat <pawankamat129@gmail.com>
2025-10-24 18:46:36 +0530

    Fixed Build Errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -8,10 +9,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link href="https://api.fontshare.com/v2/css?f[]=general-sans@400&f[]=sentient@400&f[]=gambarino@400&f[]=aktura@400&f[]=tanker@400&display=swap" rel="stylesheet"></link>
-      <script src="https://kit.fontawesome.com/4b1022cf8e.js" crossOrigin="anonymous"></script>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=general-sans@400&f[]=sentient@400&f[]=gambarino@400&f[]=aktura@400&f[]=tanker@400&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+
+        {/* Load FontAwesome asynchronously and safely */}
+        <Script
+          src="https://kit.fontawesome.com/4b1022cf8e.js"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
